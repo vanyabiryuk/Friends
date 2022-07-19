d3ea2555426b650331ee580de45b735418120148
Ivan Biryuk <vanyabiryuk@icloud.com>
2022-07-19 16:49:03 +0500

    Initial commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Vanüsha on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
